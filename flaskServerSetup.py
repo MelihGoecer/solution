31dbbfe7cf931dff742eb1ddcbecad4888afa39b
Melih Göcer <melih.pb@gmail.com>
2025-08-20 11:38:52 +0200

    Add files via upload

---
+++ flaskServerSetup.py
@@ -0,0 +1,10 @@
+from flask import Flask
+import socket
+setup = Flask(__name__)
+@setup.route('/test', methods=['GET'])
+def print_hostname():
+	hostname = socket.gethostname()
+	return f"<h1>{hostname}</h1>"
+
+if __name__ == "__main":
+	setup.run(host="localhost", port=8001)
